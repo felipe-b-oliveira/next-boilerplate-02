359f510c04ed12970d4961895563438e68cc1e4e
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-08-17 02:00:41 -0300

    feat: add initial styles

--- src/components/Main/main.tsx
+++ src/components/Main/main.tsx
@@ -1,7 +1,12 @@
+import * as Styles from './main.styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <Styles.Wrapper>
+    <Styles.Logo src='/img/logo.svg' alt='Imagem de um átomo e React Avançado escrito ao lado.' />
+    <Styles.Title>React Avançado</Styles.Title>
+    <Styles.Description>TypeScript, ReactJS, NextJS e Styled Components</Styles.Description>
+    <Styles.Illustration src='/img/hero-illustration.svg' alt='Um desenvolvedor de frente para uma tela com código.' />
+  </Styles.Wrapper>
 )
 
 export default Main
