50216ffb96f745323234ac2a6dc868b41f82191e
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-08-18 00:48:18 -0300

    feat(main.stories.tsx): remove knobs and update to Storybook controls

--- src/components/Main/main.stories.tsx
+++ src/components/Main/main.stories.tsx
@@ -1,22 +1,23 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { withKnobs, text } from '@storybook/addon-knobs'
 
 import Main from './main'
 
 const meta: Meta<typeof Main> = {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'Storybook - React Avançado',
+    description: 'Storybook - TypeScript, ReactJS, NextJS e Styled Components'
+  },
+  argTypes: {
+    title: Text,
+    description: Text
+  }
 }
 
 export default meta
 type Story = StoryObj<typeof Main>
 
 export const Basic: Story = {
-  render: () => (
-    <Main
-      title={text('Title', 'React Avançado')}
-      description={text('Description', 'TypeScript, ReactJS, NextJS e Styled Components')}
-    />
-  )
+  render: (args) => <Main {...args} />
 }
